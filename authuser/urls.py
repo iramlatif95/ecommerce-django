80a10ddc019c9b4302c984385d29a1977e65bfbd
iramlatif95 <iramlatif32@gmail.com>
2025-11-15 02:58:57 -0800

    Add password reset functionality

--- authuser/urls.py
+++ authuser/urls.py
@@ -26,6 +26,12 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'
     ), name='password_reset_complete'),
+
+    path('reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ), 
+        name='password_reset_confirm'),
 ]
 
 
